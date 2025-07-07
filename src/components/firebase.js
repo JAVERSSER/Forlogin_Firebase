8c6cf5eb1db177647af040f0d274d6f01a8ba915
Thirith Heng <thirithheng@gmail.com>
2025-07-07 15:57:30 +0700

    init

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,15 +10,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyCcYDLdMi53cAxQE6uDSq0hRAQYE5L_X0k",
   authDomain: "basiclogin-bc302.firebaseapp.com",
   projectId: "basiclogin-bc302",
-  storageBucket: "basiclogin-bc302.appspot.com", // <-- fix here
+  storageBucket: "basiclogin-bc302.firebasestorage.app",
   messagingSenderId: "7578565990",
   appId: "1:7578565990:web:37649ccf1c2026d1b9dca7",
-  measurementId: "G-BTB3LB0F1G",
+  measurementId: "G-BTB3LB0F1G"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
-export const db = getFirestore(app);
+export const auth=getAuth();
+export const db=getFirestore(app);
 export default app;
